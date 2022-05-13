5599dc3667b7aefee92a4f6c5d5958b3e409f231
Ernest Bruno <erndobruno@gmail.com>
2022-05-12 20:36:38 -0700

    Apply fixes that updates state correctly as well as moving the  functionsorts the messages inside fetch call.

--- client/src/components/ActiveChat/ActiveChat.js
+++ client/src/components/ActiveChat/ActiveChat.js
@@ -1,84 +1,71 @@
-import React, {useMemo} from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import { Box } from "@material-ui/core";
-import { Input, Header, Messages } from "./index";
-import moment from "moment";
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Box } from '@material-ui/core';
+import { Input, Header, Messages } from './index';
 
 const useStyles = makeStyles(() => ({
-    root: {
-        display: "flex",
-        flexGrow: 8,
-        flexDirection: "column",
-    },
-    chatContainer: {
-        marginLeft: 41,
-        marginRight: 41,
-        display: "flex",
-        flexDirection: "column",
-        flexGrow: 1,
-        justifyContent: "space-between",
-    },
+	root: {
+		display: 'flex',
+		flexGrow: 8,
+		flexDirection: 'column',
+	},
+	chatContainer: {
+		marginLeft: 41,
+		marginRight: 41,
+		display: 'flex',
+		flexDirection: 'column',
+		flexGrow: 1,
+		justifyContent: 'space-between',
+	},
 }));
 
 const ActiveChat = ({
-    user,
-    conversations,
-    activeConversation,
-    postMessage,
+	user,
+	conversations,
+	activeConversation,
+	postMessage,
 }) => {
-    const classes = useStyles();
+	const classes = useStyles();
 
-    const conversation = useMemo(() => conversations
-        ? conversations.find(
-              (conversation) =>
-                  conversation.otherUser.username === activeConversation
-          )
-        : {}, [activeConversation, conversations]);
+	const conversation = conversations
+		? conversations.find(
+				(conversation) => conversation.otherUser.username === activeConversation
+		  )
+		: {};
 
-    const isConversation = (obj) => {
-        return obj !== {} && obj !== undefined;
-    };
+	const isConversation = (obj) => {
+		return obj !== {} && obj !== undefined;
+	};
 
-    const sortedMessages = useMemo(() => (
-        conversation &&
-        conversation.messages.sort((prevMessage, currMessage) => {
-            return (
-                moment(prevMessage.createdAt).valueOf() -
-                moment(currMessage.createdAt).valueOf()
-            );
-        })
-
-    ), [conversation])
-
-    return (
-        <Box className={classes.root}>
-            {isConversation(conversation) && conversation.otherUser && (
-                <>
-                    <Header
-                        username={conversation.otherUser.username}
-                        online={conversation.otherUser.online || false}
-                    />
-                    <Box className={classes.chatContainer}>
-                        {user && (
-                            <>
-                                <Messages
-                                    messages={sortedMessages}
-                                    otherUser={conversation.otherUser}
-                                    userId={user.id}
-                                />
-                                <Input
-                                    otherUser={conversation.otherUser}
-                                    conversationId={conversation.id || null}
-                                    user={user}
-                                    postMessage={postMessage}
-                                />
-                            </>
-                        )}
-                    </Box>
-                </>
-            )}
-        </Box>
-    );
+	return (
+		<Box className={classes.root}>
+			{isConversation(conversation) && conversation.otherUser && (
+				<>
+					<Header
+						username={conversation.otherUser.username}
+						online={conversation.otherUser.online || false}
+					/>
+					<Box className={classes.chatContainer}>
+						{user && (
+							<>
+								<Messages
+									messages={conversation.messages}
+									otherUser={conversation.otherUser}
+									userId={user.id}
+								/>
+								<Input
+									otherUser={conversation.otherUser}
+									conversationId={conversation.id || null}
+									user={user}
+									postMessage={postMessage}
+								/>
+							</>
+						)}
+					</Box>
+				</>
+			)}
+		</Box>
+	);
 };
 
 export default ActiveChat;
