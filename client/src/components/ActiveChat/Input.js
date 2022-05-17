7e46ceba2f047400b8d8f6c94af5696e19f6ce1b
Ernest Bruno <erndobruno@gmail.com>
2022-05-17 11:51:10 -0700

    SAVE POINT. add clickable adornment.

--- client/src/components/ActiveChat/Input.js
+++ client/src/components/ActiveChat/Input.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
-import { FormControl, FilledInput } from '@material-ui/core';
+import { FormControl, FilledInput, InputAdornment } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
+import SentimentSatisfiedAltIcon from '@material-ui/icons/SentimentSatisfiedAlt';
+import InsertPhotoIcon from '@material-ui/icons/InsertPhoto';
 
 const useStyles = makeStyles(() => ({
   root: {
@@ -13,17 +15,23 @@ const useStyles = makeStyles(() => ({
     borderRadius: 8,
     marginBottom: 20,
   },
+  adornment: {
+    color: 'gray',
+    marginRight: '10px',
+    cursor: 'pointer',
+  },
 }));
 
 const Input = ({ otherUser, conversationId, user, postMessage }) => {
   const classes = useStyles();
   const [text, setText] = useState('');
+  const [images, setImages] = useState('');
 
-  const handleChange = (event) => {
+  const handleChange = event => {
     setText(event.target.value);
   };
 
-  const handleSubmit = async (event) => {
+  const handleSubmit = async event => {
     event.preventDefault();
     const form = event.currentTarget;
     const formElements = form.elements;
@@ -38,6 +46,27 @@ const Input = ({ otherUser, conversationId, user, postMessage }) => {
     setText('');
   };
 
+  const handleFileChoose = event => {
+    setImages(event.target.files);
+  };
+
+  const inputAdornment = (
+    <InputAdornment position="end">
+      <SentimentSatisfiedAltIcon className={classes.adornment} />
+      <input
+        style={{ display: 'none' }}
+        id="image-upload"
+        type="file"
+        onChange={handleFileChoose}
+        accept="image/*"
+        multiple
+      />
+      <label htmlFor="image-upload">
+        <InsertPhotoIcon className={classes.adornment} />
+      </label>
+    </InputAdornment>
+  );
+
   return (
     <form className={classes.root} onSubmit={handleSubmit}>
       <FormControl fullWidth hiddenLabel>
@@ -48,6 +77,7 @@ const Input = ({ otherUser, conversationId, user, postMessage }) => {
           value={text}
           name="text"
           onChange={handleChange}
+          endAdornment={inputAdornment}
         />
       </FormControl>
     </form>
